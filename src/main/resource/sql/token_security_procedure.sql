60047115af29e222d1c012c85e8b1edfc388c526
戚羿辰 <416350144@qq.com>
2018-04-18 18:49:17 +0800

    add init sql

---
+++ src/main/resource/sql/token_security_procedure.sql
@@ -0,0 +1,66 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : test-local
+Source Server Version : 50505
+Source Host           : 192.168.15.31:3306
+Source Database       : token_security_procedure
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2018-04-18 18:47:19
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `account`
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` int(1) NOT NULL COMMENT '1ETH',
+  `address` varchar(64) NOT NULL,
+  `private_key` varchar(255) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_admin` int(1) NOT NULL DEFAULT '0',
+  `mission_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for `mission`
+-- ----------------------------
+DROP TABLE IF EXISTS `mission`;
+CREATE TABLE `mission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` int(1) NOT NULL DEFAULT '0' COMMENT '1创建用户 2签名',
+  `total` int(10) NOT NULL,
+  `complete` int(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `token_type` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for `orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` varchar(64) NOT NULL,
+  `token_type` varchar(32) NOT NULL,
+  `value` decimal(20,5) NOT NULL,
+  `to_address` varchar(64) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `mission_id` bigint(20) DEFAULT NULL,
+  `signature` varchar(512) DEFAULT NULL,
+  `from_address` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
