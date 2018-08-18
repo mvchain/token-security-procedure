86e190d46f5843e6a3b156a9dbf55e1b04e878f6
戚羿辰 <416350144@qq.com>
2018-08-18 16:42:38 +0800

    update sql

--- src/main/resource/sql/tsp.sql
+++ src/main/resource/sql/tsp.sql
@@ -4,13 +4,13 @@ Navicat MySQL Data Transfer
 Source Server         : test-local
 Source Server Version : 50505
 Source Host           : 192.168.15.31:3306
-Source Database       : token_security_procedure
+Source Database       : tsp
 
 Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-04-18 18:47:19
+Date: 2018-08-18 16:39:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,14 +24,16 @@ CREATE TABLE `account` (
   `type` int(1) NOT NULL COMMENT '1ETH',
   `address` varchar(64) NOT NULL,
   `private_key` varchar(255) NOT NULL,
-  `pass` varchar(64) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_admin` int(1) NOT NULL DEFAULT '0',
   `mission_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=314 DEFAULT CHARSET=utf8mb4;
 
+-- ----------------------------
+-- Records of account
+-- ----------------------------
 -- ----------------------------
 -- Table structure for `mission`
 -- ----------------------------
@@ -45,25 +47,30 @@ CREATE TABLE `mission` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `token_type` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4;
 
+-- ----------------------------
+-- Records of mission
+-- ----------------------------
 -- ----------------------------
 -- Table structure for `orders`
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `order_id` varchar(64) NOT NULL,
+  `order_id` varchar(64) DEFAULT NULL,
   `token_type` varchar(32) NOT NULL,
-  `value` decimal(20,5) NOT NULL,
+  `value` decimal(30,10) NOT NULL,
   `to_address` varchar(64) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `mission_id` bigint(20) DEFAULT NULL,
   `signature` varchar(512) DEFAULT NULL,
   `from_address` varchar(64) NOT NULL,
+  `nonce` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4;
 
-
-alter table orders add column `nonce` bigint NOT NULL after `id`;
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
